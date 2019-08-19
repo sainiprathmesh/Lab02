bfd272199f0cb1724be3f6ba533865f679566e1e
Prathmesh Saini <prathmesh.saini_cs18@gla.ac.in>
2019-08-19 15:10:55 +0530

    add input,check and print statement

--- src/question4/Question4.java
+++ src/question4/Question4.java
@@ -1,7 +1,18 @@
 package question4;
 
+import java.util.Scanner;
+
 public class Question4 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int inputnumber = scanner.nextInt();
+        if (inputnumber == 0) {
+            System.out.println("Zero");
+        } else if (inputnumber > 0) {
+            System.out.println("Positive");
+        } else {
+            System.out.println("Negative");
+        }
+        scanner.close();
     }
 }
