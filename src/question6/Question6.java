b54b828cfedba7654deae83746420421c9f1ad18
Prathmesh Saini <prathmesh.saini_cs18@gla.ac.in>
2019-08-19 15:23:10 +0530

    add scanner

--- src/question6/Question6.java
+++ src/question6/Question6.java
@@ -1,7 +1,10 @@
 package question6;
 
+import java.util.Scanner;
+
 public class Question6 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
     }
 }
