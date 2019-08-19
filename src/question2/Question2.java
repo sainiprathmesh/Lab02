bb6856a9efae8f4cf290b40fb96ed5b4d60165bc
Prathmesh Saini <prathmesh.saini_cs18@gla.ac.in>
2019-08-19 14:45:12 +0530

    add input and output statement

--- src/question2/Question2.java
+++ src/question2/Question2.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 public class Question2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        String value1 = scanner.nextLine().trim();
+        String value2 = scanner.nextLine().trim();
+        System.out.println(value1 + " University " + value2);
+        scanner.close();
     }
 }
