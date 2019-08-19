541a1035c3157aa24b791f695d8a8a12e33c4ba6
Prathmesh Saini <prathmesh.saini_cs18@gla.ac.in>
2019-08-19 14:53:26 +0530

    add input statement

--- src/question3/Question3.java
+++ src/question3/Question3.java
@@ -2,8 +2,11 @@ package question3;
 
 import java.util.Scanner;
 
-public class Question2 {
+public class Question3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int value1 = scanner.nextInt();
+        int value2 = scanner.nextInt();
+
     }
 }
