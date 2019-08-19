d2ca4b755bb734684eb8627994b7417b5d2c0406
Prathmesh Saini <prathmesh.saini_cs18@gla.ac.in>
2019-08-19 15:26:14 +0530

    add input statement

--- src/question6/Question6.java
+++ src/question6/Question6.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Question6 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        char char1 = scanner.nextLine().trim().charAt(0);
+        char char2 = scanner.nextLine().trim().charAt(0);
     }
 }
