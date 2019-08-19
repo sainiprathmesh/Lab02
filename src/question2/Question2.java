807f335fce4cb5aaa3eebeeea5292aaa41dd82ae
Prathmesh Saini <prathmesh.saini_cs18@gla.ac.in>
2019-08-19 14:42:52 +0530

    add statement Scanner

--- src/question2/Question2.java
+++ src/question2/Question2.java
@@ -1,4 +1,10 @@
 package question2;
 
+import java.util.Scanner;
+
 public class Question2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
 }
