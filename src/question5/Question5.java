567afba52a35d90008b68dd63bd099a0d6ed5c21
Prathmesh Saini <prathmesh.saini_cs18@gla.ac.in>
2019-08-19 15:19:21 +0530

    add input, check and print statement

--- src/question5/Question5.java
+++ src/question5/Question5.java
@@ -5,6 +5,11 @@ import java.util.Scanner;
 public class Question5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int number = scanner.nextInt();
+        if (number % 2 == 0) {
+            System.out.println("even");
+        } else {
+            System.out.println("Odd");
+        }
     }
 }
